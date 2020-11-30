3ed94e333f3003b2fef88364d72b1c4cb7eed817
Wahyu Haryanto <way3haryanto@gmail.com>
2020-11-30 14:07:37 +0700

    first setup sapper

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	// import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		max-width: 420px;
+		margin: 0 auto;
+		background-color: #EFF1F6;
+	}
+	p {
+		padding: 0;
+	}
+</style>
+
+<!-- <Nav {segment}/> -->
+
+<main>
+	<slot></slot>
+</main>
