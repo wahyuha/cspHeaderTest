20b409ac321579573e4e77811a7fd2da8b823804
Wahyu Haryanto <way3haryanto@gmail.com>
2020-12-28 13:59:37 +0700

    Create http base abstraction

--- src/utils/http/server.js
+++ src/utils/http/server.js
@@ -1,19 +1,5 @@
-import axios from 'axios';
-
-export default class httpServer {
-  constructor() {
-    this.apiUrl;
-    this.headers;
-    this.instance;
-    this.timeout = 120000;
-
-    this
-      .createApiUrl()
-      .createHeaders()
-      .createInstance()
-      .createInterceptor()
-  }
-
+import httpBase from './base';
+export default class httpServer extends httpBase {
   createApiUrl() {
     this.apiUrl = 'http://prahu-svc.linkaja.dev'
     return this
@@ -24,41 +10,6 @@ export default class httpServer {
       'X-Request-ID': '01ERY9050186RN1VRTQZTA76BX'
     }
     return this
-  }
-
-  addHeaders(headers) {
-    this.headers = {
-      ...this.headers,
-      ...headers
-    }
-    return this
-  }
-
-  createInstance() {
-    this.instance = axios.create({
-      baseURL: this.apiUrl,
-      timeout: this.timeout,
-      headers: this.headers
-    });
-    return this
-  }
-
-  createInterceptor() {
-    this.instance.interceptors.response.use(function (response) {
-      // put interceptor here, could be log, etc
-      return response;
-    }, function (error) {
-      return Promise.reject(error);
-    });
-    return this
-  }
-
-  get(url) {
-    return this.instance.get(url)
-  }
-
-  post(url, data) {
-    return this.instance.post(url, data)
   }
 
   static addHeaders(headers) {
