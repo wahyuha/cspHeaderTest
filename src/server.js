440aae82c99f0f287146a4b1771cf28c9b580ed6
Wahyu Haryanto <way3haryanto@gmail.com>
2021-01-15 14:15:24 +0700

    Add security header with helmet

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import sirv from "sirv";
 import express from "express";
+import helmet from "helmet";
 import { json, urlencoded } from "body-parser";
 import session from "express-session";
 import compression from "compression";
@@ -16,31 +17,49 @@ const dev = NODE_ENV === "development";
 const static_path =
   NODE_ENV === "development" ? "static" : "__sapper__/build/static";
 
-app
-  .use(
-    json({ limit: "2mb" }),
-    urlencoded({ limit: "2mb", extended: true }),
-    session(sessionStore),
-    initSession(),
-    reqDecrypt(),
-    resEncrypt(),
-    compression({ threshold: 0 }),
-    sirv(static_path, { dev }),
-    sapper.middleware({
-      session: (req) => {
-        return {
-          sessionId: req.session.sessionId,
-          extSessionId: req.session.extSessionId,
-          requestId: req.session.requestId,
-          aesKey: req.session.aesKey,
-          aesDel: req.session.aesDel,
-          rsaDel: req.session.rsaDel,
-          // customerState: req.session.customerState,
-          // customerNumber: req.session.customerNumber,
-        };
-      },
-    })
-  )
-  .listen(PORT, (err) => {
-    if (err) console.log("error", err);
-  });
+// security header
+app.use(helmet.contentSecurityPolicy());
+app.use(helmet.referrerPolicy({ policy: "strict-origin-when-cross-origin" }));
+app.use(helmet.noSniff());
+app.use(helmet.hidePoweredBy());
+app.use(
+  helmet.frameguard({
+    action: "sameorigin",
+  })
+);
+app.use(
+  helmet.hsts({
+    maxAge: 15552000,
+  })
+);
+app.use((req, res, next) => {
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  next();
+});
+
+app.use(
+  json({ limit: "2mb" }),
+  urlencoded({ limit: "2mb", extended: true }),
+  session(sessionStore),
+  compression({ threshold: 0 }),
+  sirv(static_path, { dev }),
+  initSession(),
+  reqDecrypt(),
+  resEncrypt(),
+  sapper.middleware({
+    session: (req) => {
+      return {
+        sessionId: req.session.sessionId,
+        extSessionId: req.session.extSessionId,
+        requestId: req.session.requestId,
+        aesKey: req.session.aesKey,
+        aesDel: req.session.aesDel,
+        rsaDel: req.session.rsaDel,
+      };
+    },
+  })
+)
+
+app.listen(PORT, (err) => {
+  if (err) console.log("error", err);
+});
