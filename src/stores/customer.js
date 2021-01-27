1d4ecc6c25214f1f517cba15e7548360f14f434c
Wahyu Haryanto <way3haryanto@gmail.com>
2021-01-27 16:12:45 +0700

    Remove console log

--- src/stores/customer.js
+++ src/stores/customer.js
@@ -3,6 +3,5 @@ import { writable } from "svelte/store";
 export const customer = writable({});
 
 export const setCustomerNumber = (customerNumber) => {
-  console.log('set :', customerNumber);
   customer.update(c => ({ number: customerNumber }));
 };
