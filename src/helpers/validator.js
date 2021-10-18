459c46d983bca0695552cc11fb20efe2fba12840
Wahyu Haryanto <way3haryanto@gmail.com>
2021-10-18 11:47:21 +0700

    fix: remove regex single quote in name validator

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -60,7 +60,7 @@ const isValidPin = createValidator(
 
 const isValidName = createValidator(
   (message) => (value) => {
-    if (value && !/^[a-zA-Z'\s]*$/.test(value)) {
+    if (value && !/^[a-zA-Z\s]*$/.test(value)) {
       return message;
     }
   },
