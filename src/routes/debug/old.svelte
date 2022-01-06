8e24f8946d5af23156976daaea6b9539d4ba4421
Wahyu Haryanto <way3haryanto@gmail.com>
2022-01-06 09:09:59 +0700

    debugger old browser

---
+++ src/routes/debug/old.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+
+  let title = "Check old browser";
+  let description = "";
+  let partner = "LinkAja";
+
+  let counter = 0;
+
+  $: {
+    (() => {
+      counter = counter + 2;
+    })();
+  }
+
+  onMount(async () => {
+    counter++;
+  });
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="linkaja_partner" content={partner} />
+  <meta name="linkaja_title" content={title} />
+  <meta name="linkaja_description" content={description} />
+</svelte:head>
+<div class="wrapper">
+  <h1>Hello, Sempu</h1>
+  <p>
+    Counter: {counter}
+  </p>
+</div>
+
+<style>
+</style>
